d47adf6f574731dd2026b783d01b714747b3cd5d
Nel Rondón <nelucho.nel@gmail.com>
2025-11-03 23:16:12 -0400

    cambios en el HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,10 @@
-function HomePage(){
-  return(
-    <h1>Inicio | Home</h1>
-  )
+function HomePage() {
+  return (
+    <>
+      <h1>Inicio | Home</h1>
+      <h2>Bienvenido</h2>
+    </>
+  );
 }
 
 export default HomePage;
-
